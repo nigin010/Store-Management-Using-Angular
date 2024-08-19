52af8d52fa1d6886ce6c61c53eb0375f204d6b36
Nigin N Manayil <niginnmanayil10@gmail.com>
2024-08-19 10:19:33 +0530

    feat : added empty cart functionality

--- src/app/components/customer/view-cart/view-cart.component.ts
+++ src/app/components/customer/view-cart/view-cart.component.ts
@@ -16,25 +16,31 @@ export class ViewCartComponent implements OnInit {
   name: string | null = '';
   cartItems!: CartItem[];
   emptyCart: boolean = false;
+
   constructor(
     private customerService: CustomerService,
     private sanitizer: DomSanitizer,
     private readonly router: Router
   ) {}
+
   ngOnInit(): void {
     this.emptyCart = false;
     this.name = sessionStorage.getItem('name');
     if (!this.name) {
       this.router.navigate(['/login']);
     }
+    this.viewCart();
+  }
+
+  viewCart() {
     this.customerService
       .viewCart(this.name)
       .subscribe((response: DefaultResponse<CartItem>) => {
         this.cartItems = response.data;
-        console.log('Length is --> ', this.cartItems.length, this.emptyCart);
         if (this.cartItems.length === 0) this.emptyCart = true;
       });
   }
+
   getSafeImageUrl(url: string): SafeResourceUrl {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
